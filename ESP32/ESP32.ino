59e1b40d1634e79a5497887763e5c392139070de
Jack Chen <jackqchen@outlook.com>
2025-11-23 21:27:50 +0800

    Only PS4

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,27 +1,7 @@
 #include <ps4.h>
 #include <ps4_int.h>
 #include "esp_camera.h"
-#include <WiFi.h>
 #include <PS4Controller.h>
-#include <ESP32Servo.h>
-
-/* 摄像头引脚定义 - AI_THINKER模型 */
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
 
 /* 电机和LED引脚定义 */
 extern int DRV_A = 12;  // 左履带控制引脚1
@@ -35,78 +15,15 @@ extern bool ledOn = false; // LED状态
 // PS4手柄MAC地址 - 需要替换为你的PS4手柄MAC地址
 const char* PS4_MAC = "0a:62:28:18:1b:50";
 
-// AP设置
-const char* AP_SSID = "TANK-AP";
-
 // 摇杆控制相关变量
 int leftSpeed = 0;   // 左履带速度
 int rightSpeed = 0;  // 右履带速度
 const int DEADZONE = 10; // 摇杆死区值
 const int MAX_SPEED = 255; // 最大速度值
 
-void startCameraServer();
-
 void initLed() {
 	ledcSetup(7, 5000, 8); // 5000 Hz PWM, 8-bit分辨率，范围0-255
 	ledcAttachPin(ledPin, 7);
-}
-
-void setupCamera() {
-	camera_config_t config;
-	config.ledc_channel = LEDC_CHANNEL_0;
-	config.ledc_timer = LEDC_TIMER_0;
-	config.pin_d0 = Y2_GPIO_NUM;
-	config.pin_d1 = Y3_GPIO_NUM;
-	config.pin_d2 = Y4_GPIO_NUM;
-	config.pin_d3 = Y5_GPIO_NUM;
-	config.pin_d4 = Y6_GPIO_NUM;
-	config.pin_d5 = Y7_GPIO_NUM;
-	config.pin_d6 = Y8_GPIO_NUM;
-	config.pin_d7 = Y9_GPIO_NUM;
-	config.pin_xclk = XCLK_GPIO_NUM;
-	config.pin_pclk = PCLK_GPIO_NUM;
-	config.pin_vsync = VSYNC_GPIO_NUM;
-	config.pin_href = HREF_GPIO_NUM;
-	config.pin_sscb_sda = SIOD_GPIO_NUM;
-	config.pin_sscb_scl = SIOC_GPIO_NUM;
-	config.pin_pwdn = PWDN_GPIO_NUM;
-	config.pin_reset = RESET_GPIO_NUM;
-	config.xclk_freq_hz = 20000000;
-	config.pixel_format = PIXFORMAT_JPEG;
-
-	// 根据是否有PSRAM初始化相应的配置
-	if (psramFound()) {
-		config.frame_size = FRAMESIZE_UXGA;
-		config.jpeg_quality = 10;
-		config.fb_count = 2;
-	}
-	else {
-		config.frame_size = FRAMESIZE_SVGA;
-		config.jpeg_quality = 10;
-		config.fb_count = 1;
-	}
-
-	// 初始化摄像头
-	esp_err_t err = esp_camera_init(&config);
-	if (err != ESP_OK) {
-		Serial.printf("Camera init failed, Error code: 0x%x\n", err);
-		return;
-	}
-
-	// 降低分辨率以提高初始帧率
-	sensor_t* s = esp_camera_sensor_get();
-	s->set_framesize(s, FRAMESIZE_QVGA);
-}
-
-void setupWiFiAP() {
-	WiFi.mode(WIFI_AP);
-	WiFi.softAP(AP_SSID);
-	IPAddress myIP = WiFi.softAPIP();
-	Serial.println("WiFi AP Started");
-	Serial.print("AP IP: ");
-	Serial.println(myIP);
-	Serial.print("AP Name: ");
-	Serial.println(AP_SSID);
 }
 
 void setupPS4Controller() {
@@ -197,10 +114,8 @@ void toggleLED() {
 
 void setup() {
 	Serial.begin(115200);
-	Serial.setDebugOutput(true);
 	Serial.println();
-	Serial.println("*ESP32 Camera Tank - PS4*");
-	Serial.println("--------------------------------------------------------");
+	Serial.println("*Crazy Tank - PS4*");
 
 	// 设置电机控制引脚为输出
 	pinMode(DRV_A, OUTPUT);
@@ -219,20 +134,8 @@ void setup() {
 	// 初始化LED
 	initLed();
 
-	//// 设置PS4控制器
-	//setupPS4Controller();
-
-	// 初始化摄像头
-	setupCamera();
-
-	// 设置WiFi AP模式
-	setupWiFiAP();
-
-	// 启动摄像头服务器
-	startCameraServer();
-	Serial.print("Camera Ready! Use 'http://");
-	Serial.print(WiFi.softAPIP());
-	Serial.println("' to view the video");
+	// 设置PS4控制器
+	setupPS4Controller();
 }
 
 void loop() {
@@ -259,5 +162,5 @@ void loop() {
 	}
 
 	// 小延迟以减少CPU负载
-	delay(10);
+	delay(1);
 }
